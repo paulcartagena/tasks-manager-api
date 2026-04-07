fc72bad8e5013f06a64be58f51f22db61961f9bb
paulcartagena <paulcartagena04@gmail.com>
2026-04-06 22:09:02 -0600

    Configure HTTPS in OpenAPI

--- src/main/java/com/tasksmanager/api/config/OpenApiConfig.java
+++ src/main/java/com/tasksmanager/api/config/OpenApiConfig.java
@@ -5,9 +5,12 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableMBeanExport;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -15,6 +18,9 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("https://tasks-manager-api-production.up.railway.app")
+                ))
                 .info(new Info()
                               .title("Task Manager API")
                               .version("1.0.0")
@@ -24,6 +30,6 @@ public class OpenApiConfig {
                                     .addSecuritySchemes("Bearer Authentication", new SecurityScheme()
                                             .type(SecurityScheme.Type.HTTP)
                                             .scheme("bearer")
-                                            .bearerFormat("jWT")));
+                                            .bearerFormat("JWT")));
     }
 }
